d0f697fe7cbae446da522876ac6e8a36622a118b
fifiteen82726 <fifiteen82726@gmail.com>
2017-04-28 17:50:51 +0800

    read port value

--- main.js
+++ main.js
@@ -54,6 +54,7 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
   }
+  console.log('active')
 })
 
 // In this file you can include the rest of your app's specific main process
